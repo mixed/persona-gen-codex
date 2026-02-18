108a4fe64827c11309d84fb8f27ab5c5cc34453e
YongWoo Jeon <i.nevalose@gmail.com>
2026-02-18 18:19:50 +0900

    Strengthen Halton extended-base unit coverage

--- src/sampling/HaltonSampler.ts
+++ src/sampling/HaltonSampler.ts
@@ -3,18 +3,36 @@ import type { AxisCoordinate, DiversityAxis } from '../types';
 const DEFAULT_BASES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 
 export class HaltonSampler {
+  private readonly extendedBases: number[] = [];
+
   public sample(axes: DiversityAxis[], count: number): AxisCoordinate[] {
     return Array.from({ length: count }, (_, i) => {
       const index = i + 1;
       const coordinates: AxisCoordinate = {};
 
       axes.forEach((axis, axisIndex) => {
-        const base = DEFAULT_BASES[axisIndex] ?? this.nextPrime(DEFAULT_BASES[DEFAULT_BASES.length - 1] + axisIndex);
+        const base = this.getBaseForAxis(axisIndex);
         coordinates[axis.key] = this.halton(index, base);
       });
 
       return coordinates;
     });
+  }
+
+  private getBaseForAxis(axisIndex: number): number {
+    if (axisIndex < DEFAULT_BASES.length) {
+      return DEFAULT_BASES[axisIndex];
+    }
+
+    const extendedIndex = axisIndex - DEFAULT_BASES.length;
+
+    while (this.extendedBases.length <= extendedIndex) {
+      const previousBase =
+        this.extendedBases[this.extendedBases.length - 1] ?? DEFAULT_BASES[DEFAULT_BASES.length - 1];
+      this.extendedBases.push(this.nextPrime(previousBase + 1));
+    }
+
+    return this.extendedBases[extendedIndex];
   }
 
   private halton(index: number, base: number): number {
